91027925f2544dab854b101bacd03dc9d6504cf6
kidneee <k.shi8519@gmail.com>
2025-12-18 13:12:22 +0900

    portfolio-site

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const hamburgerBtn = document.getElementById('hamburgerMenu');
+const drawerMenu = document.getElementById('drawerMenu');
+
+hamburgerBtn.addEventListener('click', () => {
+  const isOpen = hamburgerBtn.getAttribute('aria-expanded') === 'true';
+  hamburgerBtn.setAttribute('aria-expanded', !isOpen);
+  hamburgerBtn.classList.toggle('open');
+  drawerMenu.classList.toggle('open');
+});
